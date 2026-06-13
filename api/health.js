d446e1ff1b7b70d543b62d1d3bf376644f887f1a
A-corrine <zz951588@gmail.com>
2026-06-13 22:02:56 +0800

    Add api/health.js

---
+++ api/health.js
@@ -0,0 +1,9 @@
+const { getConfig, sendJson } = require("./_openai-image");
+
+module.exports = function handler(req, res) {
+  if (req.method !== "GET") {
+    return sendJson(res, 405, { error: "Method not allowed" });
+  }
+
+  return sendJson(res, 200, { ok: true, model: getConfig().model });
+};
